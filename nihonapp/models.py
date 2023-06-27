e2b2ae9915cc81cbf4239a96c52aee0a27e8ca93
Manuel <ma.figueroap@duocuc.cl>
2023-06-26 20:27:55 -0400

    Initial commit

---
+++ nihonapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.IntegerField()
+    categoria = models.CharField( max_length=50)
+    descripcion = models.TextField()
+
+
+    
